01911d9d6d9047c9eae9a7e7ad9f806994460c82
Rostyslav <rostyk.holoven@gmail.com>
2020-12-04 18:24:07 +0200

    add correct version of lab2

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,77 @@
 import pandas as pd
+import geopandas as gpd
+import openpyxl as pyxl
 import plotter as plt
+
+
+def group_date(df):
+    df = df.set_index("zvit_date")
+    # Згрупувати дані по ознаці “однакова дата”
+    df_date = df.groupby(["zvit_date"]).sum()
+    print("Grouped by date")
+    print(df_date)
+    return df_date
+
+
+def group_area(df):
+    # Вибрати дані по одній з областей України
+    area = input("Choose area: ")
+    df_area = df.loc[df["registration_area"] == area]
+    new_df = group_date(df_area)
+    print(f"Grouped by {area} area")
+    print(new_df)
+    return new_df, area
+
+
+def merge_data(df_gpd, df_data, sh_option):
+    if sh_option == 1:
+        df_gpd = df_gpd.replace({"Chernihiv": "Чернігівська",
+                           "Donets'k": "Донецька",
+                           "Dnipropetrovs'k": "Дніпропетровська",
+                           "Luhans'k": "Луганська",
+                           "L'viv": "Львівська",
+                           "Ivano-Frankivs'k": "Івано-Франківська",
+                           "Odessa": "Одеська",
+                           "Poltava": "Полтавська",
+                           "Kiev": "Київська",
+                           "Chernivtsi": "Чернівецька",
+                           "Kharkiv": "Харківська",
+                           "Kirovohrad": "Кіровоградська",
+                           "Volyn": "Волинська",
+                           "Rivne": "Рівненська",
+                           "Transcarpathia": "Закарпатська",
+                           "Zhytomyr": "Житомирська",
+                           "Vinnytsya": "Вінницька",
+                           "Mykolayiv": "Миколаївська",
+                           "Kiev City": "м. Київ",
+                           "Ternopil'": "Тернопільська",
+                           "Cherkasy": "Черкаська",
+                           "Kherson": "Херсонська",
+                           "Khmel'nyts'kyy": "Хмельницька",
+                           "Sumy": "Сумська",
+                           "Zaporizhzhya": "Запорізька"})
+        left = df_gpd.rename(columns={"NAME_1": "registration_area"})
+
+    if sh_option == 2:
+        df_data = df_data.replace({"м. Київ": "Київська"})
+        left = df_gpd.rename(columns={"ADM1_UA": "registration_area"})
+
+    df_data = df_data.groupby(["zvit_date", "registration_area"]).sum().reset_index()
+    right = df_data.groupby("registration_area").sum()
+    res = left.merge(right, how="left", on="registration_area")
+    return res
+
+
+def to_excel(data):
+    name = input("Enter a filename:")
+    area_stats = data.groupby("registration_area").sum()
+    unique = data["registration_area"].unique()
+    with pd.ExcelWriter(name) as writer:
+        area_stats.to_excel(writer, sheet_name="Area_statistics")
+        for area in unique:
+            curr = data[data["registration_area"] == area].describe()
+            curr.to_excel(writer, sheet_name=area)
+    return name
 
 
 if __name__ == '__main__':
@@ -8,18 +80,38 @@ if __name__ == '__main__':
 
     # Парсимо CSV
     url = "https://raw.githubusercontent.com/VasiaPiven/covid19_ua/master/covid19_by_area_type_hosp_dynamics.csv"
-    df = pd.read_csv(url, encoding='utf-8', parse_dates=True, index_col="zvit_date")
-    print(df)
-    print(df.info(), "\n")
+    df_data = pd.read_csv(url, encoding='utf-8', parse_dates=True)
+    df_compare = df_data.groupby(["zvit_date", "registration_area"]).sum().reset_index()
+    # Зчитуємо Shapefile
+    sh_option = int(input("Choose shapefile:\n1 - with Kyiv\n2 - only areas\n"))
+    if sh_option == 1:
+        df_gpd = gpd.read_file("zip://UKR_adm.zip!UKR_adm1.shp", encoding='utf-8')
 
-    # Вибрати дані по одній з областей України
-    area = input("Виберіть область: ")
-    df_lviv = df.loc[df["registration_area"] == area]
+    if sh_option == 2:
+        df_gpd = gpd.read_file('zip://ukr_admbnda_adm1_q2_sspe_20171221.zip!ukr_admbnda_adm1_q2_sspe_20171221.shp', encoding='utf-8')
 
-    # Згрупувати дані по ознаці “однакова дата”
-    df_date = df_lviv.groupby(["zvit_date"]).sum()
-    print(df_date)
-    print(df_date.info(), "\n")
-    # Викликаємо модуль для побудови графіків
-    plt.plot_options(df_date)
+    while True:
+        print("Main menu:")
+        option = int(input("1 - group by date\n2 - group by area\n"
+                           "3 - plot module\n4 - export to excel\n"))
 
+        if option == 1:
+            # Викликаємо функцію для групування даних лише по даті
+            df_grouped = group_date(df_data)
+            area = "Україна"
+
+        if option == 2:
+            # Викликаємо функцію для групування даних по даті і області
+            df_grouped, area = group_area(df_data)
+            area += " область"
+
+        if option == 3:
+            df_merged = merge_data(df_gpd, df_data, sh_option)
+            # Викликаємо модуль для побудови графіків
+            try:
+                plt.plot_options(df_compare, df_grouped, df_merged, area)
+            except NameError:
+                raise Exception("Firstly group the data")
+
+        if option == 4:
+            print("Dataframe is written to", to_excel(df_data))
--- lab2/plotter.py
+++ lab2/plotter.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (13, 6)
+
+color_maps = {"active_confirm": "Reds", "new_death": "Greys", "new_confirm": "Oranges", "new_susp": "Blues", "new_recover": "Greens"}
 
 
-def mega_lin_sca_plot_builder(types_data, data, kind):
-    # Наводимо красу
-    plt.rcParams["figure.figsize"] = (13, 6)
-    lbl = ' / '.join([str(elem) for elem in types_data])  # підпис для осі y
+# Лінійний та точковий графік
+def lin_sca_plot_builder(data, kind, area):
+    # Вибираємо потрібні дані
+    types_data = list(map(str, input(
+        "new_susp / new_confirm / active_confirm "
+        "/ new_death / new_recover\n").split(" / ")))
+
     lay = 0
 
     # Параметр накладання графіків
@@ -14,62 +20,77 @@ def mega_lin_sca_plot_builder(types_data, data, kind):
 
     for i in types_data:
         if kind == "lin":
-            plt.plot(data.index, data[i].cumsum(), label=i + " distribution")
-            plt.xlabel('zvit_date')
-            plt.legend()
+            plt.plot(data.index, data[i], label=i)
 
         elif kind == "sca":
-            plt.scatter(data.index, data[i], label=i + " distribution", alpha=0.75)
-            plt.xlabel('zvit_date')
-            plt.legend()
+            plt.scatter(data.index, data[i], label=i, alpha=0.75)
 
-        # Формуємо графіки віносно накладання
-        if lay:
-            plt.ylabel(lbl)
-        elif not lay:
-            plt.ylabel(i)
-            plt.show()
-    if lay:
-        plt.show()
-
-
-def custom_plot_builder(data):
-    plt.rcParams["figure.figsize"] = (13, 6)
-    cnt = int(input("How many plots? "))
-    lay = 0
-    if cnt > 1:
-        lay = bool(int(input("Overlay plots?\n0 - No\n1 - Yes\n")))
-    for i in range(cnt):
-        x = input("Choose data for x: ")
-        y = input("Choose data for y: ")
-        plt.scatter(data[x], data[y], label=x + " and " + y + " distribution")
-        plt.xlabel(x)
-        plt.ylabel(y)
+        plt.xlabel('Date')
+        plt.ylabel("People")
         plt.legend()
+        plt.title(area)
+        # Формуємо графіки віносно накладання
         if not lay:
             plt.show()
     if lay:
         plt.show()
 
 
+# Графіки по мапі України
+def map_plot_builder(map_df):
+    # Вибираємо потрібні дані
+    types_data = list(map(str, input(
+        "new_susp / new_confirm / active_confirm "
+        "/ new_death / new_recover\n").split(" / ")))
+
+    for i in types_data:
+        map_df.plot(cmap=color_maps[i], column=i, edgecolor="0.5", figsize=(12.0, 8.0),
+                    missing_kwds={"color": "lightgrey", "hatch": "//"})
+        v_min, v_max = map_df[i].min(), map_df[i].max()
+        sm = plt.cm.ScalarMappable(cmap=color_maps[i],
+                                   norm=plt.Normalize(vmin=v_min, vmax=v_max))
+        plt.colorbar(sm)
+        plt.title("Ukraine COVID-19 " + i)
+        plt.axis("off")
+    plt.show()
+
+
+def compare_plot(data):
+    print(data)
+    types_area = list(map(str, input("Enter areas, please: ").split(" / ")))
+    n = len(types_area)
+    area_list = []
+    for i in range(n):
+        area_list.append(data[data["registration_area"] == types_area[i]])
+    print(area_list)
+    # Львівська / Київська
+    prop = input("Enter property, please: ")
+
+    ax = area_list[0].plot(x="zvit_date", y=prop, label=types_area[0])
+
+    for i in range(1, n):
+        area_list[i].plot(x="zvit_date", y=prop, label=types_area[i], ax=ax)
+    plt.xlabel('Date')
+    plt.legend()
+    plt.ylabel(prop)
+    plt.show()
+
+
 # Запитуємо у користувача влстивості графіків
-def plot_options(plot_data):
+def plot_options(plot_compare, plot_data, plot_map, area):
+    print(area)
     print("Choose type of plot:")
-    option = int(input("1 - Linear\n2 - Scatter\n3 - Custom\n"))
-    print("Choose the necessary data for plots:")
+    option = int(input("1 - Linear\n2 - Scatter\n3 - Map\n4 - Compare\n"))
     if option == 1:
         kind = "lin"
-        types_data = list(map(str, input(
-            "new_susp / new_confirm / active_confirm "
-            "/ new_death / new_recover - distribution by datetime\n").split(" / ")))
-        mega_lin_sca_plot_builder(types_data, plot_data, kind)
+        lin_sca_plot_builder(plot_data, kind, area)
 
     if option == 2:
         kind = "sca"
-        types_data = list(map(str, input(
-            "new_susp / new_confirm / active_confirm "
-            "/ new_death / new_recover - distribution by datetime\n").split(" / ")))
-        mega_lin_sca_plot_builder(types_data, plot_data, kind)
+        lin_sca_plot_builder(plot_data, kind, area)
 
     if option == 3:
-        custom_plot_builder(plot_data)
+        map_plot_builder(plot_map)
+
+    if option == 4:
+        compare_plot(plot_compare)
